c53efc7bb95039548aaf5ab21ab22d099a1ecb70
JoanR17 <joanr.m.1999@gmail.com>
2022-01-11 12:05:19 +0100

    Ejercicio 1

--- Tasques UD4-1/src/Ej1.java
+++ Tasques UD4-1/src/Ej1.java
@@ -3,7 +3,13 @@ public class Ej1 {
 
 	public static void main(String[] args) {
 
+		int x = 17, y = 8;
 		
+		System.out.println("Suma de " + x + " + " + y + " = " + (x + y));
+		System.out.println("Resta de " + x + " - " + y + " = " + (x - y));
+		System.out.println("Multiplicación de " + x + " * " + y + " = " + (x * y));
+		System.out.println("División de " + x + " / " + y + " = " + (x / y));
+		System.out.println("Módulo de " + x + " % " + y + " = " + (x % y));
 
 	}
 
