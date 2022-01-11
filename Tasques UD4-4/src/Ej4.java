186d41274f346b4198d5a3f02777f726ae38e96c
JoanR17 <joanr.m.1999@gmail.com>
2022-01-11 13:07:44 +0100

    Ejercicio 4

---
+++ Tasques UD4-4/src/Ej4.java
@@ -0,0 +1,22 @@
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+
+		int n = 8;
+		
+		System.out.println("Valor inicial de N = " + n); //Valor inicial
+		
+		n += 77; // Sumamos a la variable 77
+		System.out.println("N + 77 = " + n);
+		
+		n -= 3; // Restamos a la variable 3
+		System.out.println("N - 3 = " + n);
+		
+		n *= 2; // Duplicamos el valor de la variable
+		System.out.println("N * 2 = " + n);
+		
+
+	}
+
+}
