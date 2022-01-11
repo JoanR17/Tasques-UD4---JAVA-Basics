0f1067c6a4698e8254086f3931b472094f81fef4
JoanR17 <joanr.m.1999@gmail.com>
2022-01-11 12:26:54 +0100

    Ejercicio 2

---
+++ Tasques UD4-2/src/Ej2.java
@@ -0,0 +1,19 @@
+
+public class Ej2 {
+
+	public static void main(String[] args) {
+
+		int n = 8;
+		double a = 5.75;
+		char c = 'j';
+		
+		System.out.println("Variable N = " + n);
+		System.out.println("Variable A = " + a);
+		System.out.println("Variable C = " + c);
+		System.out.println(n + " + " + a + " = " + (n + a));
+		System.out.println(a + " - " + n + " = " + (a - n));
+		System.out.println("Valor numérico del carácter " + c + " = " + (int)c);
+
+	}
+
+}
